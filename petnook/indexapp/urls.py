2bf6981124ff2541a166a819404643d20d07136e
rekha <rekhaelizabethmathew21@gmail.com>
2024-09-07 07:39:21 +0530

    chatroom done

--- petnook/indexapp/urls.py
+++ petnook/indexapp/urls.py
@@ -5,16 +5,9 @@ from.import views
 from django.conf import settings
 urlpatterns = [
     path('',views.index,name='index'),
-    path('contact/',views.contact,name='contact'),
     path('movies/',views.movies,name='movies'),
-    path('productdescription/<int:movie_id>/',views.productdescription,name='productdescription'),
-
-    
-    
     path('directors/',views.directors,name='directors'),
     path('director/<int:director_id>/movies/', views.director_movies, name='director_movies'),
-
-    
     path('movie/<int:movie_id>/', views.movie_detail, name='movie_detail'),
 
     
--- petnook/indexapp/views.py
+++ petnook/indexapp/views.py
@@ -9,46 +9,10 @@ from django.db.models import Avg
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
-def contact(request):
-    return render(request,'contact.html')
-def seller(request):
-    return render(request,'seller.html')
-def sellerpage(request):
-    return render(request,'sellerpage.html')
+    movies = Movie.objects.annotate(average_rating=Avg('ratings__rating')).order_by('-average_rating')[:3]
+    return render(request,'index.html', {'movies': movies})
 
-def productdescription(request,movie_id):
-    movie = get_object_or_404(Movie, id=movie_id)
 
-    # Check if the user has already rated or reviewed this movie
-    user_rating = Rating.objects.filter(movie=movie, user=request.user).first()
-    user_review = Review.objects.filter(movie=movie, user=request.user).first()
-
-    # Calculate average rating
-    average_rating = Rating.objects.filter(movie=movie).aggregate(Avg('rating'))['rating__avg']
-
-    if request.method == 'POST':
-        review_text = request.POST.get('review_text')
-        rating_value = request.POST.get('rating')
-
-        if review_text and not user_review:
-            # Save the new review
-            Review.objects.create(movie=movie, user=request.user, review_text=review_text)
-
-        if rating_value and not user_rating:
-            # Save the new rating
-            Rating.objects.create(movie=movie, user=request.user, rating=int(rating_value))
-
-        return redirect('movie_detail', movie_id=movie.id)
-
-    context = {
-        'movie': movie,
-        'average_rating': average_rating,
-        'reviews': movie.reviews.all(),
-        'user_review': user_review,
-        'user_rating': user_rating,
-    }
-    return render(request,'productdescription.html',context)
 def movies(request):
     
     movie_list = Movie.objects.all()
